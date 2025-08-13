2014308d14c06c37400945419cd1f33a3da2d616
shree <shree@ITHAcer>
2025-08-13 18:51:42 +0530

    update login script

--- SonchafaApplication/src/main/java/com/test/LoginPage.java
+++ SonchafaApplication/src/main/java/com/test/LoginPage.java
@@ -1,26 +1,53 @@
 package com.test;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
+	public static void main(String[] args) throws InterruptedException {
 
-	public WebDriver login() {
+		// Disable Chrome save password popup
+		ChromeOptions options = new ChromeOptions();
+		Map<String, Object> prefs = new HashMap<>();
+		prefs.put("profile.password_manager_leak_detection", false);
+		prefs.put("profile.Credentials_enable_service", false);
+
+		options.setExperimentalOption("prefs", prefs);
+
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\shree\\Downloads\\Test file\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+
+		WebDriver driver = new ChromeDriver(options);
+
+		// WebDriver driver = new ChromeDriver();
 
 		driver.manage().window().maximize();
-		driver.get("https://ith.sonchafaresorts.com/site/index.php");
+		driver.get("http://192.168.29.5/sonchafa/site/index.php");
 
-		driver.findElement(By.id("email")).sendKeys("bk@test.com");
-		driver.findElement(By.id("password")).sendKeys("12345678");
+		driver.findElement(By.id("email")).sendKeys("anup@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("Anup@123");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 
 		System.out.println("Page Title: " + driver.getTitle());
 		System.out.println("Current URL: " + driver.getCurrentUrl());
-		return driver;
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		System.out.println();
+		// Click through the menu
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("icon-apps"))).click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//i[@class='fa fa-clipboard']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("nav_add_user_role"))).click();
+
+		System.out.println("Done!");
 	}
 
 }
--- SonchafaApplication/src/main/java/com/test/Users.java
+++ SonchafaApplication/src/main/java/com/test/Users.java
@@ -10,18 +10,19 @@ public class Users {
     public static void main(String[] args) throws InterruptedException {
         // Create LoginPage object
         LoginPage lg = new LoginPage();
-       
         
         // Call login method and get the same driver session
-        WebDriver driver = lg.login(); // <-- Make sure LoginPage method returns WebDriver
-        driver.switchTo().alert().dismiss();
+      //  WebDriver driver = lg.login();  // <-- Make sure LoginPage method returns WebDriver
+       // driver.switchTo().alert().dismiss();
         // Create explicit wait
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+      
 
         // Click through the menu
-        wait.until(ExpectedConditions.elementToBeClickable(By.id(""))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.id(""))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.id(""))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("icon-apps"))).click();
+       
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//i[@class='fa fa-clipboard']"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("nav_add_user_role"))).click();
 
         System.out.println("Done!");
        // driver.quit();
